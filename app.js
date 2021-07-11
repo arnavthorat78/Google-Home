229a48ac089e00dd853a207e478a8187da02deca
timmythesheep78 <arnavthorat78@gmail.com>
2021-07-11 10:07:43 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const toggleSteps = document.querySelector(".toggleSteps");
+const steps = document.querySelector(".steps");
+
+const google = document.querySelector(".google");
+const response = document.querySelector(".response");
+
+steps.style.visibility = "hidden";
+const toggleRedirectInstructions = () => {
+	if (steps.style.visibility == "hidden") {
+		steps.style.visibility = "visible";
+	} else {
+		steps.style.visibility = "hidden";
+	}
+};
+
+toggleSteps.addEventListener("click", toggleRedirectInstructions);
+
+google.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	let userCommand = google.command.value;
+
+	if (userCommand.toLowerCase() == "hello") {
+		response.innerHTML = "Hello!";
+		setTimeout(() => {
+			response.innerHTML = "";
+		}, 5000);
+	} else {
+		open(
+			`https://www.google.com/search?q=${encodeURIComponent(
+				userCommand
+			)}&oq=Google&aqs=chrome..69i57j35i39l2j69i60l5.1335j0j1&sourceid=chrome&ie=UTF-8`,
+			"_blank"
+		);
+
+		response.innerHTML = `Opened <i>${userCommand}</i>.`;
+		setTimeout(() => {
+			response.innerHTML = "";
+		}, 5000);
+	}
+});
