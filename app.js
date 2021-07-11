4548fa8409ee25c83ab6dc4d3c0bd058b8b1b186
timmythesheep78 <arnavthorat78@gmail.com>
2021-07-11 15:11:36 +1000

    Added Time component

--- app.js
+++ app.js
@@ -5,12 +5,68 @@ const google = document.querySelector(".google");
 const response = document.querySelector(".response");
 const smartNote = document.querySelector(".smartNote");
 
-steps.style.visibility = "hidden";
+const time = document.querySelector(".time");
+
+const instructions = `
+    <span> To do this, preform the following steps depending on your browser: </span>
+    <br /><br />
+
+    <strong>For Google Chrome:</strong>
+    <ul>
+        <span>1. Navigate to the button next to the URL (site information).</span>
+        <br />
+        <span>2. On the bottom, click <i>Site Settings</i>.</span>
+        <br />
+        <span>3. Scroll to <i>Pop-ups and redirects</i> and click <i>Allow</i>.</span>
+    </ul>
+
+    <strong>For Microsoft Edge:</strong>
+    <ul>
+        <span>1. Navigate to the button next to the URL (site information).</span>
+        <br />
+        <span>2. Click <i>Permissions for this site</i> (with a password icon on it).</span>
+        <br />
+        <span>3. Scroll to <i>Pop-ups and redirects</i> and click <i>Allow</i>.</span>
+    </ul>
+
+    <strong>For Firefox:</strong>
+    <ul>
+        <span>
+            1. Navigate to the three horizontal lines (on the right side of the toolbar),
+            and click <i>Settings</i>.
+        </span>
+        <br />
+        <span>
+            2. Navigate to <i>Privacy and Security</i> on the left-hand side toolbar.
+        </span>
+        <br />
+        <span>
+            3. Scroll down to <i>Permissions</i>, and next to <i>Block pop-up windows</i>,
+            click <i>Exceptions...</i>.
+        </span>
+        <br />
+        <span>
+            4. Type in the URL of the website to allow (which is <u class="website">${document.URL}</u>).
+            Then click <i>Allow</i>.
+        </span>
+    </ul>
+
+    <strong>This website does not support Internet Explorer.</strong>
+`;
+
+let instructionsDisplayed = false;
 const toggleRedirectInstructions = () => {
-	if (steps.style.visibility == "hidden") {
-		steps.style.visibility = "visible";
+	if (!instructionsDisplayed) {
+		instructionsDisplayed = true;
+		steps.innerHTML = instructions;
+		window.scroll({
+			top: innerHeight,
+			left: 0,
+			behavior: "smooth",
+		});
 	} else {
-		steps.style.visibility = "hidden";
+		instructionsDisplayed = false;
+		steps.innerHTML = "";
 	}
 };
 
@@ -28,20 +84,29 @@ const smartReact = (command) => {
 		.trim()
 		.toLowerCase();
 
-	let randNum = Math.random();
-	let number;
-	if (randNum <= 0.5) {
-		number = 0;
-	} else {
-		number = 1;
+	if (command == "hello") {
+		res("Hello!", 5000);
 	}
+	if (command == "time" || command == "clock") {
+		res("The time will be displayed below.", 5000);
 
-	if (command == "hello" || "hi") {
-		if (number == 0) {
-			res("Hello!", 5000);
-		} else {
-			res("How you doing?", 5000);
-		}
+		const tick = () => {
+			const now = new Date();
+
+			time.innerHTML = `${dateFns.format(now, "h")} : ${dateFns.format(
+				now,
+				"mm"
+			)} : ${dateFns.format(now, "ss")} ${dateFns.format(now, "A")}`;
+		};
+
+		alert("To remove the time, double-click on the time.");
+
+		const interval = setInterval(tick, 1000);
+
+		time.addEventListener("dblclick", () => {
+			clearInterval(interval);
+			time.innerHTML = "";
+		});
 	}
 };
 
@@ -50,7 +115,7 @@ toggleSteps.addEventListener("click", toggleRedirectInstructions);
 google.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	let userCommand = google.command.value.trim();
+	let userCommand = google.command.value;
 	let parameters = google.params.value;
 
 	if (parameters == "Search") {
