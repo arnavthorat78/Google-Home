c9046125313e169003675f9bdbc78b78f225b1d2
arnavthorat78 <arnavthorat78@gmail.com>
2021-07-15 17:00:19 +1000

    Added better form validation

--- signUp.js
+++ signUp.js
@@ -1,19 +1,29 @@
 const signUp = document.querySelector(".signUp");
-const response = document.querySelector(".response");
+const feedback = document.querySelector(".feedback");
+
+const passwordPattern = /^[a-zA-Z0-9]{5,}$/;
 
 signUp.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	let firstName = signUp.firstName.value;
 	let lastName = signUp.lastName.value;
+	let username = signUp.username.value;
 	let email = signUp.email.value;
 	let password = signUp.password.value;
-	let confirmPassword = signUp.confirmPassword.value;
 
-	if (!(password == confirmPassword)) {
-		response.innerHTML = "Please make sure that both passwords match.";
-		setTimeout(() => {
-			response.innerHTML = "";
-		}, 5000);
+	if (passwordPattern.test(password)) {
+		feedback.textContent = "Your password is valid!";
+	} else {
+		feedback.textContent =
+			"The password must contain letters and numbers only (no symbols), and longer than 5 characters.";
+	}
+});
+
+signUp.password.addEventListener("keyup", (e) => {
+	if (passwordPattern.test(e.target.value)) {
+		signUp.password.setAttribute("class", "success");
+	} else {
+		signUp.password.setAttribute("class", "error");
 	}
 });
