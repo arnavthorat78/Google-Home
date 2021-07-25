51f4414e7031caf34e127c2d7473cf59f8f3c4be
arnavthorat78 <arnavthorat78@gmail.com>
2021-07-25 14:34:46 +1000

    Added info about Voice Commands

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const user = document.querySelector(".user");
 const greeting = document.querySelector(".greeting");
 const scrollUp = document.querySelector(".scrollUp");
 
-let randNum = Math.ceil(Math.random() * 5);
+let randNum = Math.ceil(Math.random() * 10);
 
 let fullNameRaw = localStorage.getItem("user");
 if (fullNameRaw == null) {
@@ -13,6 +13,8 @@ if (fullNameRaw == null) {
 	let fullName = `${splitFullName[0]} ${splitFullName[1]}`;
 	user.innerHTML = fullName;
 
+	const now = new Date().getHours();
+
 	if (randNum == 1) {
 		greeting.innerHTML = `Welcome back, ${firstName}!`;
 	} else if (randNum == 2) {
@@ -21,6 +23,20 @@ if (fullNameRaw == null) {
 		greeting.innerHTML = `Let's start searching, ${firstName}!`;
 	} else if (randNum == 4) {
 		greeting.innerHTML = `What should we do, ${firstName}?`;
+	} else if (randNum == 5 || randNum == 6) {
+		if (now >= 0 && now < 12) {
+			greeting.innerHTML = `Good morning, ${firstName}!`;
+		} else if (now >= 12 && now < 17) {
+			greeting.innerHTML = `Good afternoon, ${firstName}!`;
+		} else {
+			greeting.innerHTML = `Good evening, ${firstName}!`;
+		}
+	} else if (randNum == 7) {
+		greeting.innerHTML = `Glad to see you again, ${firstName}!`;
+	} else if (randNum == 8) {
+		greeting.innerHTML = `What to do, ${firstName}?`;
+	} else if (randNum == 9) {
+		greeting.innerHTML = `Welcome, ${firstName}!`;
 	} else {
 		greeting.innerHTML = `Hi, ${firstName}!`;
 	}
