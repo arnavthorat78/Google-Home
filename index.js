fa777d89fa5d1b2f6efe95e5eba7c5a91b0106dc
arnavthorat78 <arnavthorat78@gmail.com>
2021-08-07 15:11:00 +1000

    Added better Toast functionality

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+// Getting references to elements in the DOM
 const user = document.querySelector(".user");
 const greeting = document.querySelector(".greeting");
+const hours = document.querySelector(".hoursSinceRelease");
 const scrollUp = document.querySelector(".scrollUp");
 
+// Getting a random number for later use
 let randNum = Math.ceil(Math.random() * 10);
 
+// Getting the user's name from local storage
 let fullNameRaw = localStorage.getItem("user");
 if (fullNameRaw == null) {
+	// If the name is nothing (which means the user doesn't have an account), then set the navbar name to User
 	user.innerHTML = "User";
 } else {
+	// Spliting the name, and appending it to the navbar
 	const splitFullName = fullNameRaw.split(",");
 	let firstName = splitFullName[0];
 	let fullName = `${splitFullName[0]} ${splitFullName[1]}`;
 	user.innerHTML = fullName;
 
+	// Getting the hours for later use
 	const now = new Date().getHours();
 
+	// Random greetings
 	if (randNum == 1) {
 		greeting.innerHTML = `Welcome back, ${firstName}!`;
 	} else if (randNum == 2) {
@@ -42,6 +50,13 @@ if (fullNameRaw == null) {
 	}
 }
 
+// Time since
+// TODO Get a better formula
+let updateTime = new Date("August 6, 2021 17:00:00").getHours();
+let now = new Date().getHours();
+hours.innerHTML = `${24 - updateTime + now} hours ago`;
+
+// Making the window scroll to the top when button clicked
 scrollUp.addEventListener("click", () => {
 	window.scroll({
 		top: 0,
